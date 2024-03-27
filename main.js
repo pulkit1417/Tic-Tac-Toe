60af00dd16a0b23c9cdaebb6cbf899130c68481d
pulkit1417 <gupta.pulkit2408@gmail.com>
2024-03-27 18:25:23 +0530

    animation added

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 let boxes = document.querySelectorAll("#box");
 let resetBtn = document.querySelector("#reset-btn");
 let newGameBtn = document.querySelector("#new-btn");
+let newBtnDraw = document.querySelector("#new-btn2");
 let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
+let msg2 = document.querySelector("#msg2");
+let draw1 = document.querySelector(".draw");
 
-let turn0 = true;//PlayerX , PlayerO
-let count=0;// To Track Draw
+let turn0 = true; //PlayerX , PlayerO
+let count = 0; // To Track Draw
 
 let winPatterns = [
   [0, 1, 2],
@@ -20,27 +23,29 @@ let winPatterns = [
 
 const resetGame = () => {
   turn0 = true;
-  count=0;
+  count = 0;
+  // document.body.style.backgroundColor = "#0077b6";
   EnabledBoxes();
   msgContainer.classList.add("hide");
+  draw1.classList.add("hide2");
 };
 
 boxes.forEach((box) => {
   box.addEventListener("click", () => {
     if (turn0) {
       box.innerText = "O";
-      box.style.color="#ECA400";
+      box.style.color = "#ECA400";
       turn0 = false;
     } else {
       box.innerText = "X";
-      box.style.color="#BC3908";
+      box.style.color = "#BC3908";
       turn0 = true;
     }
     box.disabled = true;
     count++;
 
-    let isWinner=checkWinner();
-    if(count == 9 && !isWinner){
+    let isWinner = checkWinner();
+    if (count == 9 && !isWinner) {
       draw();
     }
   });
@@ -62,12 +67,13 @@ const disabledBoxes = () => {
 const showWinner = (winner) => {
   msg.innerText = `Congratulations ,Winner is ${winner}`;
   msgContainer.classList.remove("hide");
+  // document.body.style.backgroundColor = "black";
   disabledBoxes();
 };
 
-const draw = () =>{
-  msg.innerText =`The game ended in a draw.`
-  msgContainer.classList.remove("hide");
+const draw = () => {
+  msg2.innerText = `The game ended in a draw.`;
+  draw1.classList.remove("hide2");
   disabledBoxes();
 };
 
@@ -87,3 +93,4 @@ const checkWinner = () => {
 
 newGameBtn.addEventListener("click", resetGame);
 resetBtn.addEventListener("click", resetGame);
+newBtnDraw.addEventListener("click", resetGame);
